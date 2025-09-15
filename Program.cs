27f77c7534d403961024d13276a9ef701815d092
Konstantin Sl <kostatop2@gmail.com>
2025-09-15 14:26:38 +0300

    Added new dict and changed the path to saved img

--- Program.cs
+++ Program.cs
@@ -8,14 +8,66 @@ class Program
 {
     static void Main()
     {
-        string path = @"C:\Users\kosta\source\repos\TextToImage\TextToImage\King_Stiven_Zelenaya_milya.txt";
-        string text = readFile(path);
+        string bookName = "King_Stiven_Zelenaya_milya.txt";
+        string path = @"C:\Users\kosta\source\repos\TextToImage\TextToImage\";
+        string text = readFile(path+bookName);
         text = text.ToLower();
         getColorsFromText(text);
-        bitmap.Save("шахматная_доска.png", ImageFormat.Png);
+        bitmap.Save(path+"BookPortrait.png", ImageFormat.Png);
     }
-    static Dictionary<string, Color> dict = new Dictionary<string, Color> { { "красн", Color.Red }, { "оранжев", Color.Orange }, { "желт", Color.Yellow }, { "зелен", Color.Green }, { "голуб", Color.LightBlue }, { "син", Color.Blue }, { "фиолетов", Color.Purple }, { "багров", Color.Crimson }, { "черн", Color.Black }, { "бел", Color.White } };
-    static Bitmap bitmap = new Bitmap(1000, 1000);
+    static Dictionary<string, Color> dict = new Dictionary<string, Color>
+{
+    { "красн", Color.Red },
+    { "оранжев", Color.Orange },
+    { "желт", Color.Yellow },
+    { "зелен", Color.Green },
+    { "голуб", Color.LightBlue },
+    { "син", Color.Blue },
+    { "фиолетов", Color.Purple },
+    { "багров", Color.Crimson },
+    { "черн", Color.Black },
+    { "бел", Color.White },
+    { "розов", Color.Pink },
+    { "коричнев", Color.Brown },
+    { "сер", Color.Gray },
+    { "серебр", Color.Silver },
+    { "золот", Color.Gold },
+    { "лазурн", Color.Azure },
+    { "бирюзов", Color.Turquoise },
+    { "небесн", Color.SkyBlue },
+    { "морск", Color.SteelBlue },
+    { "оливков", Color.Olive },
+    { "лайм", Color.Lime },
+    { "мятн", Color.MintCream },
+    { "лесн", Color.ForestGreen },
+    { "индиго", Color.Indigo },
+    { "орхиде", Color.Orchid },
+    { "лососев", Color.Salmon },
+    { "томатн", Color.Tomato },
+    { "шоколадн", Color.Chocolate },
+    { "пшеничн", Color.Wheat },
+    { "льнян", Color.Linen },
+    { "медн", Color.Tan },
+    { "бежев", Color.Beige },
+    { "персиков", Color.PeachPuff },
+    { "кораллов", Color.Coral },
+    { "магн", Color.Magenta },
+    { "фукси", Color.Fuchsia },
+    { "лаванд", Color.Lavender },
+    { "сливов", Color.Plum },
+    { "сиренев", Color.Thistle },
+    { "аквамарин", Color.Aquamarine },
+    { "бисквитн", Color.Bisque },
+    { "ванильн", Color.BlanchedAlmond },
+    { "кремов", Color.Cornsilk },
+    { "медов", Color.Honeydew },
+    { "слонов", Color.Ivory },
+    { "льдист", Color.AliceBlue },
+    { "дымчат", Color.GhostWhite },
+    { "снежн", Color.Snow },
+    { "жемчужн", Color.SeaShell }
+}; 
+    static Bitmap bitmap = new Bitmap(300, 300);
     static Graphics graphics = Graphics.FromImage(bitmap);
     static string readFile(string path)
     {
@@ -34,7 +86,7 @@ class Program
             if (foundColor != null)
             {
                 x += 10;
-                if (x == 1000)
+                if (x == 300)
                 {
                     y += 10;
                     x = 0;
